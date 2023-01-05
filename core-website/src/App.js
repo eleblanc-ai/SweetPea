35627d927fb795635c7323d1f21ddc5449854356
Emily LeBlanc <eleblanc@protonmail.com>
2023-01-05 15:51:34 -0500

    Commented core-website/src/App.js and core-server/Server.py.

--- core-website/src/App.js
+++ core-website/src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
+/* SweetPea core website */
+/* Author: Emily LeBlanc */
+/* Date: 1/5/2023 */
+
 import React, { useState } from 'react';
+
+/* Import style */
+import './App.css';
+
+/* Import core server hostname and port */
 import {SERVER_HOSTNAME, SERVER_PORT} from "./consts";
-
-
 
 function App() {
     const [testMessageString, setTestMessageString] = useState('');
 
-    // Keep it RESTful with HTTP verbs: https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/
+    /* Keep it RESTful with HTTP verbs: https://www.geeksforgeeks.org/get-and-post-method-using-fetch-api/ */
 
-    // Make request to the SweetPea core server's `testMessage` endpoint
+    /* Make GET request to the SweetPea core server's `testMessage` endpoint */
     const fetchTestMessage = () => {
         let url = "http://"+ SERVER_HOSTNAME + ":" + SERVER_PORT + "/testMessage"
         fetch(url)
@@ -21,17 +27,24 @@ function App() {
 
   return (
     <div className="App">
+
+      {/* Demo header, replace with your own -- edit style in App.css */}
       <div className="App-header">
         <p>
           SweetPea: A tiny web development kit.
         </p>
       </div>
-      <div className="App-main-content">
+        {/* Main site content -- edit style in App.css */}
+        <div className="App-main-content">
 
-          {/* Main site content */}
+          {/* Demo button -- edit style in App.css */}
+          {/* On click, demo button will call fetchTestMessage */}
           <button className="App-button"
                   type="button"
-                  onClick={fetchTestMessage}>Press for test message</button>
+                  onClick={fetchTestMessage}>Press for test message
+          </button>
+
+          {/* Display demo response -- edit style in App.css */}
           <div className="test-message-response">
               <p>{testMessageString}</p>
           </div>
